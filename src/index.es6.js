f36d4a399c8a3fe87b49c51cda337077b3e171c6
Worapot Pengsuk <plamworapot@gmail.com>
2022-03-11 17:29:51 +0700

    Initial commit

---
+++ src/index.es6.js
@@ -0,0 +1,18 @@
+import path from "path";
+import express from "express";
+
+import bundleMiddlware from "./middleware/bundle";
+import ssr from "./routes/ssr";
+
+console.log("INIT APP");
+const app = express();
+
+const publicFolder = path.resolve(__dirname, "../public");
+
+app.use(express.static(publicFolder, { index: [] }));
+
+app.use(bundleMiddlware);
+
+app.get("/*", ssr);
+
+app.listen(process.env.PORT || 8080);
